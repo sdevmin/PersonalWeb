c3565ed120480318b1ef36f67ab053990d70e746
sdevmin <sachiradevmin17@gmail.com>
2025-07-17 18:58:23 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showText() {
+    document.getElementById("hoverText").style.opacity = 1;
+  }
+  
+  function hideText() {
+    document.getElementById("hoverText").style.opacity = 0;
+  }
+  
